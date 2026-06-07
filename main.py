7bef3276fbe81f645c83f8c840a16c6360c7ee1e
ItxHarshx <itxharshx@gmail.com>
2026-06-07 18:13:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
 from telegram import Update
+from telegram.ext import MessageHandler, filters
+import asyncio
 from telegram.ext import Application, CommandHandler, ContextTypes
 import os
 
@@ -25,10 +27,29 @@ def main():
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(
+    MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, welcome)
+    )
 
     print("Bot is running...")
 
     app.run_polling()
+
+async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    for user in update.message.new_chat_members:
+
+        mention = user.mention_html(user.first_name)
+
+        msg = await update.message.reply_html(
+            f"Hello {mention}, welcome to ABESIT Batch (2026-2027). wishing you a great college journey! 🎉"
+        )
+
+        await asyncio.sleep(300)  # 5 minutes
+
+        try:
+            await msg.delete()
+        except:
+            pass
 
 
 if __name__ == "__main__":
