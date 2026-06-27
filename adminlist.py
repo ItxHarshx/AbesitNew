5df9583576761b9f4ac0b919cee36ccb075422bf
ItxHarshx <itxharshx@gmail.com>
2026-06-27 22:07:18 +0530

    Update adminlist.py

--- adminlist.py
+++ adminlist.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import ContextTypes, CommandHandler
+from telegram.ext import ContextTypes
 
 async def adminlist(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat = update.effective_chat
