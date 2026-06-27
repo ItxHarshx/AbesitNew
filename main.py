9bd5549d5ce9e7cf16428ec0cd0f6a08b784834f
ItxHarshx <itxharshx@gmail.com>
2026-06-27 21:43:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from telegram.ext import CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import MessageHandler, filters, Application, CommandHandler, ContextTypes
 from info import SUDO_USERS, GROUP_ID
+from adminlist import adminlist
 
 load_dotenv()
 
@@ -1083,48 +1084,6 @@ async def stats(update: Update, context: ContextTypes.DEFAULT_TYPE):
         f"• Official Admins: {sudo_count}\n\n"
         f"- Group Status: {status}"
     )
-
-async def adminlist(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat = update.effective_chat
-
-    if chat.type not in ("group", "supergroup"):
-        await update.message.reply_text(
-            "This command can only be used in groups."
-        )
-        return
-
-    admins = await chat.get_administrators()
-
-    owner = None
-    admin_list = []
-
-    for admin in admins:
-        user = admin.user
-
-        # clickable mention only (NO username)
-        mention = f'<a href="tg://user?id={user.id}">{user.first_name}</a>'
-
-        if admin.status == "creator":
-            owner = mention
-        else:
-            admin_list.append(mention)
-
-    text = "<b>👑 Admin List</b>\n\n"
-
-    # OWNER FIRST
-    if owner:
-        text += f"<b>👑 Owner:</b>\n{owner}\n\n"
-    else:
-        text += "<b>👑 Owner:</b> Not found\n\n"
-
-    # ADMINS LIST
-    if admin_list:
-        text += "<b>🛡 Admins:</b>\n"
-        text += "\n".join([f"• {a}" for a in admin_list])
-    else:
-        text += "<b>🛡 Admins:</b> None"
-
-    await update.message.reply_html(text)
 
 async def antilink(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global ANTILINK_ENABLED
