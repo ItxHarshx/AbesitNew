bf432d82714fa3b94c872f355cc187a65a64d7d6
ItxHarshx <itxharshx@gmail.com>
2026-06-28 15:30:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from antilink import antilink, anti_link_filter
 from admin import promote, demote
 from goodbye import member_left
 #from downloader import downloader
-from instagram import get_instagram_video
+from instagram import download_Instagram 
 
 load_dotenv()
 
