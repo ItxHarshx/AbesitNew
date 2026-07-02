5f7b85cf71328161a0770e7e68f47c89537bb1bc
ItxHarshx <itxharshx@gmail.com>
2026-07-02 08:56:40 +0530

    Update tord.py

--- tord.py
+++ tord.py
@@ -91,7 +91,8 @@ async def cmd_truthordare_info(update: Update, context: ContextTypes.DEFAULT_TYP
         "- /cancel_tord: cancel the game (admin/creator)\n"
         "- /rm_tord: reply to remove a player (admin/creator)\n"
         "- /done: confirm your turn is finished\n\n"
-        "<i> #under_dev </i>"
+        "<i>Use /truthordare for this message again</i>"
+
     )
     await update.message.reply_html(text)
 
